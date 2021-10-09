92d6a51effcf8348b3d49c4fd3298e59c15d7055
kaaverr <makciimgogol@gmail.com>
2021-10-09 10:17:53 +0300

    v2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,18 @@ import java.util.List;
 
 public class Main {
     public static int[] getZeros(int[] array){
-        return array;
+        int[] arrayOut = new int[array.length];
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] != 0){
+                list.add(array[i]);
+            }
+        }
+        for (int i = 0; i < list.size(); i++) {
+            arrayOut[i] = list.get(i);
+        }
+        System.out.println(list);
+        return arrayOut;
     }
     public static void main(String[] args) {
 	// [7, 12, 5, 0, 4, 0, 5]
@@ -15,8 +26,6 @@ public class Main {
                 7, 12, 5, 0, 4, 0, 5
         };
         System.out.println(Arrays.toString(getZeros(array)));
-
-        List<Integer> list = new ArrayList<>(array);
 
     }
 }
